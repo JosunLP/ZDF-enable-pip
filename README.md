851a2a440267bacf199895f24d6d38cac2bfcffe
Jonas Pfalzgraf <info@josunlp.de>
2023-06-29 17:35:21 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# UserScriptProjectTemplate
-A user script project template to create large and structured TypeScript projects for Tampermonkey or Greasemonkey. It is intended to form a scalable base and is primarily aimed at the Ingress community.
+# ZDF-enable-pip
+
+A smal script that enables picture in picture in the ZDF Mediathek
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
+import { T } from 'simple-toast-messages';
+
 /**
  * App
  */
 class App {
 
+    private msg: T = T.getInstance();
+
     constructor() {
         this.main();
     }
     
-    private main() {
-        console.log('Hello World!');
+    private async main():Promise<void> {
+        while (true) {
+            this.updateVideoFrames();
+            await this.sleep(1000);
+        }
+    }
+
+    private sleep(ms: number): Promise<void> {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    // get all HTML video elementsfrom the dom and remove disablepictureinpicture="true" and preload="none"
+    private updateVideoFrames():void {
+        const videos: HTMLVideoElement[] = Array.from(document.getElementsByTagName('video'));
+        videos.forEach((video: HTMLVideoElement) => {
+            video.removeAttribute('disablepictureinpicture');
+            video.removeAttribute('preload');
+        });
     }
 }
 
